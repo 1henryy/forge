f744ff97368352a750269db83960792de3ea6482
1henry <henryshang79@gmail.com>
2025-12-14 17:06:44 +0000

    parallel + distributed exports

---
+++ src/forge/distributed/__init__.py
@@ -0,0 +1,3 @@
+from .coordinator import Coordinator
+from .worker import Worker
+from .protocol import send_message, recv_message, encode_batches, decode_batches
---
+++ src/forge/parallel/__init__.py
@@ -0,0 +1,2 @@
+from .partition import PartitionStrategy, RoundRobinPartition, HashPartition
+from .executor import ParallelScanExec, ParallelAggregateExec
