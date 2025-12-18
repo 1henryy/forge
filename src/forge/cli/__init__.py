a9d5b72e2cf3aeea0c7a4d4b0a169c90059b2b16
1henry <henryshang79@gmail.com>
2025-12-18 15:06:31 +0000

    add .load and .schema dot-commands to REPL

---
+++ src/forge/cli/__init__.py
@@ -0,0 +1,2 @@
+from .repl import main
+from .explain import format_logical_plan, format_physical_plan, format_plan_tree
--- src/forge/cli/repl.py
+++ src/forge/cli/repl.py
@@ -1,4 +1,3 @@
-# __MARKER_0__
 from __future__ import annotations
 
 import sys
